92491b3efa1c2371169dbee5058a56457ad070cc
Rashid <rasvpz@gmail.com>
2022-06-14 17:23:53 +0530

    first heroku updates

--- frondend/src/Components/Header/Header.js
+++ frondend/src/Components/Header/Header.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {Navbar, Nav, FormControl, InputGroup} from 'react-bootstrap'
 import '../Header/Header.css'
 import { BsSearch, BsHeart, BsPerson, BsCartDash } from "react-icons/bs";
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
 <>
 <Navbar collapseOnSelect expand="lg" className="topNavBG" variant="dark">
-  <Navbar.Brand className="brand" href="#home"><b>freeQ</b></Navbar.Brand>
+  <Navbar.Brand className="brand">  <Link className="brnd" to ="/"><b>freeQ</b></Link> </Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
--- frondend/src/Components/ProductsPage/Products.js
+++ frondend/src/Components/ProductsPage/Products.js
@@ -22,25 +22,25 @@ const Products = () => {
                               
 
 
-<Card className="myCard" style={{marginBottom:'20px', width: '17rem' }}>
+<Card className="myCard" style={{marginBottom:'10px', width: '20rem', border:'1px solid #c0c0c0', borderRadius: '0px'}}>
   
   <Card.Body style={{padding: '0rem 0rem'}}>
   <div style={{position: 'relative'}}>
-    <img className="img-responsive" style={{width: '16.9rem'}} alt="T-Shirt" src= {'../../images/T-shirts/'+(filteredProducts.pic)} ></img>
-    <div style={{position: 'absolute', top:'57%', textAlign: 'center', color: 'white', padding:'10px', width:'100%', minHeight:'160px', backgroundImage: 'linear-gradient(transparent, #555555)'}}>
-    <Card.Title style={{ border:'1px solid white', padding:'4px'}}>{ filteredProducts.name}</Card.Title>
-    <p style={{fontSize:'16px'}}>{filteredProducts.description}  </p>
+    <img className="img-responsive" style={{width: '19.9rem'}} alt="T-Shirt" src= {'../../images/T-shirts/'+(filteredProducts.pic)} ></img>
+    <div style={{position: 'absolute', top:'63%', textAlign: 'center', color: 'white', padding:'10px', width:'100%', minHeight:'160px', backgroundImage: 'linear-gradient(transparent, #555555)'}}>
+    <Card.Title style={{ border:'1px solid white', padding:'4px', textShadow:'2px 2px grey'}}>{ filteredProducts.name}</Card.Title>
+    <p style={{fontSize:'16px', color:'#eeeded'}}>{filteredProducts.description}  </p>
     </div>  
   </div>
 
 
-    <Card.Text style={{paddingLeft: '8px'}} as='h6'><div className='my-3'> $ {filteredProducts.rate}</div></Card.Text>
+    <Card.Text style={{paddingLeft: '20px'}} as='h6'><div className='my-3'> $ {filteredProducts.rate}</div></Card.Text>
 
-    <Card.Text as='div' style={{paddingLeft: '8px', fontSize: '14px'}}>    
+    <Card.Text as='div' style={{paddingLeft: '20px', fontSize: '14px', marginTop:'-10px'}}>    
     <Rating value={filteredProducts.ratings}  text={`${filteredProducts.reviews} reviews `}/>
     </Card.Text>
 
-    <Button style={{borderRadius:'0px', marginLeft:'8px', marginBottom:'20px',  marginTop:'20px'}} variant="dark">Buy Now</Button>
+    <Button style={{borderRadius:'0px', marginRight:'14px', marginBottom:'14px',  marginTop:'20px', float: 'right'}} variant="dark">Buy Now</Button>
   </Card.Body>
 </Card>
 
